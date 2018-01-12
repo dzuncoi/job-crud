2dc5b9a22059b8a54c682379237c2ec69aab6b68
Dung Huynh <clock.quangdung@gmail.com>
2018-01-12 14:51:03 +0700

    Setup react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import logo from './logo.svg';
 import './App.css';
 
@@ -18,4 +19,6 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = state => state
+
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 import 'antd/dist/antd.css';
+
 import './index.css';
+import store from './redux/store'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = () => (
+  <Provider store={store}>
+    <Router>
+      <Route exact path="/" component={App}/>
+    </Router>
+  </Provider>
+)
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
